00ff186b0d21c831c2676417d3ab62408c5dafb7
Flxmid <f.alfredmid@gmail.com>
2025-08-19 16:20:44 +0200

    Add production configuration for basePath and assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: isProd ? '/metacraft' : '',       // vervang 'metacraft' door je repo-naam als nodig
+  assetPrefix: isProd ? '/metacraft/' : '',  // hetzelfde hier
 };
 
 module.exports = nextConfig;
